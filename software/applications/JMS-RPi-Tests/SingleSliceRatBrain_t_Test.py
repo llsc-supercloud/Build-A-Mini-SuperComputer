e72eccb94bd689ae8a157c1e0d5504f1e07dc5a6
JSMullen <julia.mullen@gmail.com>
2026-01-15 10:25:54 -0500

    adding JMS-rat-brain-example, data and code for serial to parallel example

---
+++ software/applications/JMS-RPi-Tests/SingleSliceRatBrain_t_Test.py
@@ -0,0 +1,78 @@
+# imports
+import os, math, numpy as np
+import matplotlib.pyplot as plt
+
+# set the paths to the data
+dataDir = "/Users/sullivan/Dropbox/UMass/JMS-RPi-Tests/"
+testfile = "RPiTestData001.txt"
+filename = os.path.join(dataDir, testfile)
+file_out = filename + 'out'
+nr = 0
+nc = 0
+nt = 0
+nRows = 256
+nCols = 256
+nTime = 50
+data = np.zeros((256,256,50))
+elements = np.zeros((256*50,256))
+with open(filename,'r') as f:
+    k = 0
+    for line in f:
+        elements[k,:] = line.strip().split()
+        k = k+1
+    k = 0
+    for nt in range(nTime):
+        for nr in range(nRows):
+            data[nr,:,nt] = elements[k,:]
+            k = k + 1
+
+# Get the average and standard deviation for the rows and columns through the
+#  two time periods - the control period and the post stimulus period
+sample_count = 15
+onset = 30
+start_index = onset-sample_count
+end_index = onset-1
+pre_matrix = data[:,:,start_index:end_index]
+avg_pre = np.mean(pre_matrix,axis=2)
+std_pre = np.std(pre_matrix,axis=2)
+
+start_index = onset+1
+end_index = onset+sample_count
+post_matrix = data[:,:,start_index:end_index]
+avg_post = np.mean(post_matrix,axis=2)
+std_post = np.std(post_matrix,axis=2)
+
+# now create the t-test for each row by column entry
+# since both pre and post onset have the same size
+#  we can use square-root ((std_Pre^2 + std_post^2)/2) * sqrt(2/sample_size)
+var_pre = np.square(std_pre)
+var_post = std_post*std_post
+# in case the standard deviation combinations approaches zero
+sp = np.sqrt( (var_pre+var_post)/2)
+threshold = .01
+sp[sp < threshold] = threshold
+
+t_stat = (avg_pre - avg_post)/(sp*np.sqrt(2/sample_count))
+
+# replace any location with 255 (for grayscale imaging) if the t_test was significant
+threshold = 5
+replacement_value = 0
+t_stat[np.abs(t_stat) < threshold] = replacement_value
+replacement_value = 255
+t_stat[np.abs(t_stat) > threshold] = replacement_value
+
+img_output = np.zeros((256,256))
+for nr in range(nRows):
+    img_output[nr,:] = data[:,nr,0] + t_stat[nr,:]
+
+np.savetxt(file_out,img_output,fmt='%.4d')
+#
+# Display the matrix as an image
+plt.imshow(img_output, cmap='gray', interpolation='nearest')
+plt.show(block=False)
+# Pause the display for 3 seconds
+plt.pause(3)
+# Close all plot windows automatically
+plt.close('all')
+
+
